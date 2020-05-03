37a9828903df20f0d8c7de37303ac98d7af3712d
Sanjay Suresh <59367217+RapidCompiler@users.noreply.github.com>
2020-05-03 16:39:35 +0530

    Added a feature
    Displays the word as ***** and then displays the correctly guessed letters of the word one by one. Solved issue #1

--- Hangman.py
+++ Hangman.py
@@ -17,7 +17,7 @@ while x:
 x = True
 while x:
     try:
-        b = int(input(("What maximum word length do you want? [2~15] : ")))
+        b = int(input(("What word length do you want? [2~15] : ")))
         length = int(a)
         if length < 2 or length > 15:
             x = True
@@ -27,31 +27,44 @@ while x:
     except:
         print("Please enter a valid integer for the length of the word")
 
-list = []
+lst = []
 print("\nSelecting a word")
 print("Please wait...")
-word = r.get_random_word(hasDictionaryDef="true",maxLength=b)
+word = r.get_random_word(hasDictionaryDef="true",maxLength=b,minLength=b)
 count=0
 print("\nPlease enter all letters in lower case")
 print("Do not enter the same letters repeatedly...")
 word.lower()
+word_hash = list('*' * len(word))
+print(word)
 while guess >= 0 and count <= b :
+    print("\nThe word is - ",end="")
+    for element in word_hash:
+        print(element,end="")
+    
     letter = input("\nEnter a letter : ")
-    while letter in list:
+    while letter in lst:
         print("You have already entered this letter before")
         print("Please enter a different one")
         letter = input("\nEnter a letter : ")
-    list.extend(letter)
+    lst.extend(letter)
     occ = word.count(letter)
     if occ == 0:
         print(letter,"is NOT IN the word!")
         guess -=1
     elif occ==1:
         print(letter,"is IN the word!")
+        index = word.find(letter)
+        word_hash[index] = letter
     else:
         print(letter,"is IN the word and it occurs",occ,"TIMES IN the word!")
+        for i in range(0,len(word)):
+            if word[i] == letter:
+                word_hash[i] = letter
     count += occ
     print("Attempts remaining : ",guess)
+    if count == b:
+        break
 if count == b:
     print("\nCongratulations! You have guessed the word correctly!")
     print("\nThe word is",word)
